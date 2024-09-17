e5f10ead437591deab788de5644d3426b8b49337
spite400k <spite400k@gmail.com>
2024-09-18 06:39:48 +0900

    不要行を削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { FormEvent, useRef, useState } from "react";
-import { BeakerIcon } from '@heroicons/react/24/solid'
 import ReactMarkdown from "react-markdown";
 
 // メッセージ
@@ -11,15 +10,12 @@ interface Message {
 }
 
 const MultiLineBody = ({ body }: { body: string }) => {
-  // body = body.replace(/\\n/g, '\n');
   const texts = body.split('\\n\\n').map((item, index) => {
     console.log(item);
     item = item.replace(/\\n/g, '\n');
-    // console.log("4:"+item);
     return (
       <React.Fragment key={index}>
         <ReactMarkdown>{item}</ReactMarkdown>
-        {/* <br /> */}
       </React.Fragment>
     );
   });
@@ -71,16 +67,10 @@ export default function Home() {
           buffer += chunk.slice(3, boundary-1);
           chunk = chunk.slice(boundary + 1);
           console.log("11111:"+buffer);
-          // buffer = buffer.replace(/\\n\\n/g, '\\n');
-          // console.log("22222:"+buffer);
-          // process.stdout.write(buffer);
           setOutput(buffer);
           boundary = chunk.indexOf("\n");
         }
       }
-
-      // console.log(output);
-      // process.stdout.write(output);
     } catch (e: any) {
       if (e.code === 'ECONNABORTED') {
         setError('タイムアウト: 15秒以内に回答が返ってきませんでした。');
@@ -101,8 +91,6 @@ export default function Home() {
     
     try {
       fetchStream([{ role: "user", content: input }]);
-      // console.log(output);
-      // process.stdout.write(output);
     } catch (error) {
       console.error("Error:", error);
     }
@@ -110,54 +98,7 @@ export default function Home() {
 
   return (
     <div className="mx-auto my-16 min-w-1/2 max-w-2xl px-4">
-      {/* <div className="bg-gray-700 rounded-md md:flex md:items-center md:justify-between py-4 px-4">
-        <div className="min-w-0 flex-1">
-          <h2 className="text-2xl font-bold leading-7 text-white">Next ChatBot</h2>
-        </div>
-      </div>
-      <div className="px-4">
-        <div className="py-8">
-          <label
-            htmlFor="email"
-            className="block font-medium leading-6 text-lg text-gray-900 pb-2"
-          >
-            質問フォーム：
-          </label>
-          <div className="mt-2">
-            <textarea
-              id="question"
-              className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              placeholder="質問したいことを入力してください"
-              maxLength={500}
-              rows={5}
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-            />
-          </div>
-        </div>
-        <div className="flex justify-end mb-8">
-          <button
-            className="rounded-md bg-indigo-600 px-6 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            disabled={isLoading || input.length === 0}
-            onClick={handleSubmit}
-          >
-            質問する
-          </button>
-        </div>
-            {error && <div className="mt-4 text-red-500">{error}</div>}
-            {output && (
-              <>
-                <div className="font-medium leading-6 text-lg text-gray-900 pb-2">回答：</div>
-                <p className="mt-2 text-gray-700">{output}</p>
-              </>
-            )}
-      </div> */}
-
-
-      
-
        <div className="flex h-full w-full bg-white rounded-lg shadow-lg">
-
           <div className="w-1/4 bg-gray-800 p-4 flex flex-col">
             <h2 className="text-white text-lg mb-4">チャットリスト</h2>
             <ul>
@@ -165,22 +106,17 @@ export default function Home() {
               <li className="text-white">グループ 2</li>
             </ul>
           </div>
-
-
           <div className="flex-1 flex flex-col bg-gray-100">
             <div className="flex-1 overflow-y-auto p-4">
               {error && <div className="mt-4 text-red-500">{error}</div>}
               {output && (
               <div className="markdown" >
                   <div className="font-medium leading-6 text-lg text-gray-900 pb-2">回答：</div>
-                  {/* <p className="mt-2 text-gray-700">{output}</p> */}
                   <MultiLineBody body={output} />
-                  {/* <ReactMarkdown >{output}</ReactMarkdown> */}
                 </div>
               )}
               
             </div>
-
 
             <form onSubmit={handleSubmit} className="p-4 flex items-center bg-white border-t border-gray-300">
               <input
@@ -190,23 +126,12 @@ export default function Home() {
                 onChange={(e) => setInput(e.target.value)}
                 placeholder="メッセージを入力"
               />
-              {/* <button type="button" onClick={() => fileInputRef.current.click()} className="ml-2">
-                <BeakerIcon className="h-6 w-6 text-gray-500" />
-              </button>
-              <input
-                type="file"
-                ref={fileInputRef}
-                className="hidden"
-                onChange={sendFile}
-              /> */}
               <button className="ml-2 p-2 bg-blue-500 text-white rounded-lg" type="submit">
                 送信
               </button>
             </form>
-
           </div>
         </div>
-      
     </div>
   );
 }

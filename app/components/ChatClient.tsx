b272dd356fa3e3471a7cfaecc6796fd31ee5247e
spite400k <spite400k@gmail.com>
2024-09-18 15:33:32 +0900

    修正

--- app/components/ChatClient.tsx
+++ app/components/ChatClient.tsx
@@ -4,12 +4,13 @@ import React from 'react'
 import { RecoilRoot, useRecoilState } from 'recoil'
 import ChatMessage from './ChatMessage'
 import ChatForm from './ChatForm'
+import styles from './css/chat.module.css'
 
 const ChatClient = () => {
   return (
     <RecoilRoot >
       {/* <!-- メッセージエリア --> */}
-      <div className="flex-grow overflow-auto p-6 space-y-5 ">
+      <div className="flex-grow overflow-auto p-6 space-y-5 styles.chat styles.border__chat">
           <ChatMessage />
       </div>
   
--- app/components/ChatMessage.tsx
+++ app/components/ChatMessage.tsx
@@ -2,11 +2,26 @@
 import React from 'react'
 import { useRecoilState } from 'recoil';
 import { chatLogState } from '../state/chatLogState';
+import ReactMarkdown from 'react-markdown';
+import styles from './css/chat.module.css'
 
 type MessageType = {
   id: number;
   content: string;
   sender: string;
+};
+
+const MultiLineBody = ({ body }: { body: string }) => {
+  const texts = body.split('\\n\\n').map((item, index) => {
+    console.log(item);
+    item = item.replace(/\\n/g, '\n');
+    return (
+      <React.Fragment key={index}>
+        <ReactMarkdown>{item}</ReactMarkdown>
+      </React.Fragment>
+    );
+  });
+  return <div>{texts}</div>;
 };
 
 const ChatMessage = () => {
@@ -17,15 +32,18 @@ const ChatMessage = () => {
         return (
           <div 
 	    key={message.id} 
-	    className={`flex items-end ${message.sender === 'user' ? 'justify-end' : ''}`}>
+	    className={`flex items-end ${message.sender === 'user' ? 'justify-end' : ''} styles.chat__icon`}>
             {message.sender === 'other' && (
               <div className="flex-shrink-0 mr-2">
                 <div className="h-8 w-8 bg-gray-300 rounded-full" /> {/* アイコンの代わり */}
               </div>
             )}
             <div 
-	     className={`rounded p-2 ${message.sender === 'user' ? 'bg-blue-200' : 'bg-gray-500'}`}>
-              <p className="text-sm">{message.content}</p>
+	     className={`rounded p-2 ${message.sender === 'user' ? 'bg-blue-200' : 'bg-gray-500'} styles.chat__text`}>
+              <p className="text-sm markdown">
+                {/* {message.content} */}
+                <MultiLineBody body={message.content} />
+                </p>
             </div>
           </div>
         )

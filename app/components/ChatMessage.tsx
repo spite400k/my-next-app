a9564c35f793c8b044b3220fb717e73d4e52dee0
spite400k <spite400k@gmail.com>
2024-09-22 18:39:03 +0900

    ローディングアニメーション修正

--- app/components/ChatMessage.tsx
+++ app/components/ChatMessage.tsx
@@ -40,7 +40,6 @@ const ChatMessage = () => {
     <>
       {chatLog.map((message:MessageType) => {
         return (
-          <>
             <div key={message.id} 
                   className={`mb-2 p-2 rounded-lg max-w-2xl ${
                     message.sender === 'user' ? 'flex bg-blue-300 text-white self-end' : 'flex self-start' }`}>
@@ -58,16 +57,27 @@ const ChatMessage = () => {
                 </div>
               </div>
             </div>
-          
-          </>
         )
       })}
 
       {isLoading.bool ? (
-        <div className="font-medium leading-6 text-lg text-indigo-700 pb-2">読み込み中...</div>
-      ) : (
+        <div className="mb-2 p-2 rounded-lg max-w-2xl flex self-start">  
+          <div className="flex-shrink-0 mr-2">
+            <div className="h-8 w-8 bg-black rounded-full" /> {/* アイコンの代わり */}
+          </div>
+          <div className="flex justify-start p-2" aria-label="読み込み中">
+            <div className="font-medium leading-6 text-lg text-indigo-700 pb-2 pr-3">読み込み中</div>
+            <div className="flex justify-center items-center pb-2">
+              <div className="animate-ping h-2 w-2 bg-blue-600 rounded-full"></div>
+              <div className="animate-ping h-2 w-2 bg-blue-600 rounded-full mx-4"></div>
+              <div className="animate-ping h-2 w-2 bg-blue-600 rounded-full"></div>
+            </div>
+          </div>        
+
+        </div>
+       ) : (
         <></>
-      )}
+      )} 
     </>
   )
 }

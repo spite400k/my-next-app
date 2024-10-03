ffcd88875faea6b407bad7e69ef19ded8d30554e
spite400k <spite400k@gmail.com>
2024-10-04 00:17:09 +0900

    入力欄が拡大した後、自動で縮まらないのを修正

--- app/components/ChatForm.tsx
+++ app/components/ChatForm.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { useRecoilState, useResetRecoilState } from 'recoil'
 import { chatLogState } from '../state/chatLogState'
 import { loadingState } from '../state/loadingState'
@@ -10,6 +10,8 @@ const ChatForm = () => {
   const [chatLog, setChatLog] = useRecoilState(chatLogState)
 
   const [isLoading, setIsLoading] = useRecoilState(loadingState);
+
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null); // テキストエリアの参照を管理
 
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -25,6 +27,7 @@ const ChatForm = () => {
     }
     if (e.key === 'Enter' && (e.metaKey || e.ctrlKey)) {
       doSubmit();
+
     }
   };
 
@@ -41,7 +44,11 @@ const ChatForm = () => {
     const updatedMessages = [...chatLog, newUserMessage];
     setChatLog(updatedMessages);
     setInput("");
-
+    // テキストエリアの高さを元に戻す
+    if (textareaRef.current) {
+        textareaRef.current.style.height = "auto"; // 高さをリセット
+    }
+    
     try {
       const res = await fetch(`/api/response`, {
         method: "POST",
@@ -65,30 +72,37 @@ const ChatForm = () => {
     } catch (error) {
       console.error('Error fetching GPT response:', error);
     } finally {
-      setIsLoading({ bool: false});
+      setIsLoading({ bool: false });
     }
+
+
   }
+
+    const handleInput = () => {
+    // テキストエリアの高さを動的に調整する（自動リサイズ）
+    if (textareaRef.current) {
+      textareaRef.current.style.height = "auto"; // リセット
+      // textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`; // 内容に応じて高さを調整
+      // 改行に合わせて高さを変える
+      if(textareaRef.current.scrollHeight < 300){
+        textareaRef.current.style.height = textareaRef.current.scrollHeight + 'px';
+      }else{
+        textareaRef.current.style.height = 300 + 'px';
+      }
+    }
+  };
 
   return (
     <form onSubmit={handleSubmit} className=" bottom-0 w-full p-3 bg-gray-200 flex justify-between items-center">
       <textarea
         // type="text"
         value={input}
-        onChange={(e) => {
-              // これ入れないとサイズが変わったあとに内容を削除したときなど動きがおかしい
-              e.target.style.height = 'auto';
-              // 改行に合わせて高さを変える
-              if(e.target.scrollHeight < 300){
-                e.target.style.height = e.target.scrollHeight + 'px';
-              }else{
-                e.target.style.height = 300 + 'px';
-              }
-          
-              setInput(e.target.value)
-        }}
+        onChange={(e) => {setInput(e.target.value) }}
+        onInput={handleInput}
         onKeyDown={(e)=>handleKeydown(e)} 
         className="w-full p-2 mr-2 rounded focus:outline-none text-gray-800" 
         placeholder="メッセージを入力...  ctrl+Enterでも送信できます"
+        ref={textareaRef}
       />
       <button disabled={isLoading.bool} type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         送信

2ecd0205df158fb30a27ace2618345ca79989724
spite400k <spite400k@gmail.com>
2024-11-18 09:52:02 +0900

    名称を変更

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="p-3 bg-gray-800 text-white">
-            <h1 className="text-lg">チャットルーム</h1>
+            <h1 className="text-lg">ブログ作成支援</h1>
         </div>
     )
 };

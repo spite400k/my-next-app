732b5a56e464c4550008a4056c296c0276c208a1
spite400k <spite400k@gmail.com>
2025-01-01 11:00:08 +0900

    顔アイコンを微修正

--- app/components/icons/robot.tsx
+++ app/components/icons/robot.tsx
@@ -4,24 +4,24 @@ import { motion } from 'framer-motion';
 const RobotFace = () => {
   return (
     <motion.div
-      className="w-24 h-24 bg-gray-500 rounded flex items-center justify-center relative"
+      className="w-16 h-16 bg-gray-500 rounded flex items-center justify-center relative"
     >
       {/* 左目 */}
       <motion.div
-            className="absolute top-4 left-6 w-5 h-5 bg-white rounded-full"
+            className="absolute top-4 left-3 w-3 h-3 bg-white rounded-full"
             animate={{ scaleY: [1, 0.1, 1] }}
             transition={{ duration: 1, repeat: Infinity, repeatDelay: 2 }}>
          
         </motion.div>
       {/* 右目 */}
       <motion.div
-            className="absolute top-4 right-6 w-5 h-5 bg-white rounded-full"
+            className="absolute top-4 right-3 w-3 h-3 bg-white rounded-full"
             animate={{ scaleY: [1, 0.1, 1] }}
             transition={{ duration: 1, repeat: Infinity, repeatDelay: 2 }}>
          
         </motion.div>
       {/* 口 */}
-      <div className="absolute bottom-6 w-10 h-2 bg-white rounded-md"></div>
+      <div className="absolute bottom-4 w-8 h-2 bg-white rounded-md"></div>
     </motion.div>
   );
 };

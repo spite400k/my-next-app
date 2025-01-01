8278dc85f7d41a3cc62fc600722d353c9230d397
spite400k <spite400k@gmail.com>
2025-01-01 09:11:29 +0900

    背景修正

--- app/components/chat/ChatMessage.tsx
+++ app/components/chat/ChatMessage.tsx
@@ -129,7 +129,7 @@ const ChatMessage = () => {
   return (
     <>
       <div
-        className="flex flex-col  bg-white shadow-md rounded-lg p-4 "
+        className="flex flex-col bg-white rounded-lg p-4"
         ref={chatContainerRef}  
         onScroll={handleScroll}
       >

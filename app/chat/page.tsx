e1540b1edfeab3de839c2eb150e0753d4565cef4
spite400k <spite400k@gmail.com>
2025-01-02 00:03:07 +0900

    調整

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -14,7 +14,9 @@ export default function Page() {
       <Header />
       <div className="flex flex-grow">
         <Sidebar />
-        <ChatClient />
+        <div className="flex-grow flex flex-col bg-gray-100">
+          <ChatClient />
+        </div>
       </div>
     </div>
   );

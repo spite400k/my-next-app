26541f8de0a6767dc90e70fd46fc1e75159514c2
spite400k <spite400k@gmail.com>
2025-01-26 16:25:09 +0900

    サイドバー修正

--- app/components/common/SideBar.tsx
+++ app/components/common/SideBar.tsx
@@ -1,8 +1,11 @@
 'use client'
 
 import React from 'react';
+import { useSession } from 'next-auth/react';
 
 const Sidebar = () => {
+  const { data: session } = useSession();
+
     return (
       <nav className="w-64 bg-gray-800 text-white p-4">
         <ul>
@@ -19,13 +22,14 @@ const Sidebar = () => {
             <a href="/dashboard" className="block py-2 px-4 rounded h over:bg-gray-700">ダッシュボード</a>
           </li>
           <li className="mb-2">
-            <a href="/login" className="block py-2 px-4 rounded hover:bg-gray-700">ログイン</a>
+            {(session == null) &&   <a href="/login" className="block py-2 px-4 rounded hover:bg-gray-700">ログイン</a>}
           </li>
           <li className="mb-2">
-            <a href="/signup" className="block py-2 px-4 rounded hover:bg-gray-700">会員登録</a>
+            {(session == null) &&   <a href="/signup" className="block py-2 px-4 rounded hover:bg-gray-700">会員登録</a>}
           </li>
+          
           <li className="mb-2">
-            <a href="/logout" className="block py-2 px-4 rounded hover:bg-gray-700">ログアウト</a>
+            {(session != null) &&   <a href="/logout" className="block py-2 px-4 rounded hover:bg-gray-700">ログアウト</a>}
           </li>
         </ul>
       </nav>
